c273645726b7dd7e085d803a2b7fe5033b7ddf07
aliturab99 <syedyawaraliturab@gmail.com>
2024-04-20 02:28:54 +0500

    Layout Component added

---
+++ dashboard/src/components/commonComponents/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Layout() {
+
+  return (
+    <div className='grid'>
+        <div className='col-3'>
+            <div class="text-center text-red p-3 m-3 border-round-lg bg-white font-bold ">
+
+            </div>
+        </div>
+        <div className='col-9'>
+            Col 9
+        </div>
+    </div>
+  )
+}
+
+export default Layout
